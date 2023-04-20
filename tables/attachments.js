25bfb13a8ed435133a686e43e90313b5fb7f4cf5
shazwi <supershazwi@gmail.com>
2023-04-20 19:03:01 +0800

    Remove pound

--- tables/attachments.js
+++ tables/attachments.js
@@ -27,6 +27,7 @@ const importAttachments = async () => {
       const stats = fs.statSync(`workers/${directory}/${file}`);
 
       let replacedFile = file.replace(/ /g, '-');
+      replacedFile = file.replace(/#/g, '-');
 
       const attachment = {};
       attachment.date_record_created = today;
